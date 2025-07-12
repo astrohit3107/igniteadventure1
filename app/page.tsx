6e4cbf3825c856cf61d84e00fdd943b59f871d08
Alakhshrivastava <shrivastavaalakh26@gmail.com>
2025-07-12 19:25:20 +0530

    Checking_the_branch

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function HomePage() {
     },
   ]
 
-  const indianLocations = ["Mussoorie", "Corbett", "Nainital", "Barot", "Manali", "Shimla", "Amritsar"]
+  const indianLocations = ["Mussoorie", "Corbett", "Nainital", "Barot", "Manali", "Shimla", "Amritsar","jaipur"]
 
   return (
     <div className="flex flex-col min-h-screen">
